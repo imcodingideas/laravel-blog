2950c538c87ad8b62b1cdd33af7f59c9b6935ebc
Joseph Chambers <joseph@michael-chambers.com>
2018-06-11 12:28:04 -0700

    adds comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,4 +4,40 @@
  <h1>{{ $post->title }}</h1>
 
  {{ $post->body }}
+
+<hr>
+
+ <div class="comments">
+    <ul class="list-group">
+      @foreach ($post->comments as $comment)
+        <li class="list-group-item">
+          <strong>
+            {{ $comment->created_at->diffForHumans() }}
+          </strong>
+            {{ $comment->body }}
+        </li>
+      @endforeach
+    </ul>
+ </div>
+
+ <hr>
+
+ <div class="card">
+   <div class="card-body">
+    <form method="POST" action="/posts/{{ $post->id }}/comments">
+      @csrf
+
+      <div class="form-group">
+        <textarea name="body" placeholder="Your comment here." class="form-control" required></textarea>
+      </div>
+
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Add Comment</button>
+      </div>
+
+      @include ('layouts.errors')
+    </form>
+   </div>
+ </div>
+
 @endsection
