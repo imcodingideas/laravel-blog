66b8bf7b9164717019e8205f8cab127b66146061
Joseph Chambers <joseph@michael-chambers.com>
2018-06-11 08:59:50 -0700

    single post and scss

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="/css/blog.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
   </head>
 
   <body>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,7 @@
 @extends ('layouts.master')
 
 @section ('content')
-  <h1>Hello I am a post!</h1>
+ <h1>{{ $post->title }}</h1>
+
+ {{ $post->body }}
 @endsection
